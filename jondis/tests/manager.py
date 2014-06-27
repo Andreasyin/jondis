928e4d48ca1e64ea272b63ddb5282e35a14cb707
xiaogaozi <gaochangjian@gmail.com>
2014-06-27 13:02:53 +0800

    tune for Travis CI

--- jondis/tests/manager.py
+++ jondis/tests/manager.py
@@ -21,30 +21,31 @@ class Manager(object):
         global port
         slave_of = "--slaveof 127.0.0.1 {}".format(master) if master else ""
         if getpass.getuser() == 'travis':
-            start_command = "sudo redis-server --port {} {}".format(port,
-                                                                    slave_of)
+            start_command = "sudo redis-server --port {0} {1}".format(port,
+                                                                      slave_of)
         else:
-            start_command = "redis-server --port {} {}".format(port, slave_of)
+            start_command = "redis-server --port {0} {1}".format(port,
+                                                                 slave_of)
 
         proc = subprocess.Popen(start_command, shell=True, stdout=DEVNULL,
                                 stderr=DEVNULL)
         self.procs[name] = (proc, port)
         port += 1
         # ghetto hack but necessary to find the right slaves
-        sleep(.5)
+        sleep(1)
         return self.procs[name][1]
 
     def stop(self, name):
         (proc, port) = self.procs[name]
         proc.terminate()
         # same hack as above to make sure failure actually happens
-        sleep(.5)
+        sleep(1)
 
     def promote(self, port):
         admin_conn = redis.StrictRedis('localhost', port)
-        logger.debug("Promoting {}".format(port))
+        logger.debug("Promoting {0}".format(port))
         admin_conn.slaveof()  # makes it the master
-        sleep(.5)
+        sleep(1)
 
     def shutdown(self):
         for (proc, port) in self.procs.itervalues():
